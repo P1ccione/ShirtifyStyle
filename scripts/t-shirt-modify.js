4bd746aa05ca86df2692f3d8816e42ae83ab5dba
P1ccione <simone.piscozzo@gmail.com>
2023-04-14 14:12:30 +0200

    implementazione cambio colore maglietta

---
+++ scripts/t-shirt-modify.js
@@ -0,0 +1,15 @@
+const colors = document.querySelectorAll(".fa-circle");
+const tShirt = document.querySelector(".container .t-shirt .t-shirt-img");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    setColor(color);
+  });
+});
+function setColor(color) {
+  colors.forEach((color) => {
+    color.classList.remove("active");
+  });
+  tShirt.style.backgroundImage = `url(../img/T-Shirt-uomo-${color.id}.png)`;
+  color.classList.add("active");
+}
