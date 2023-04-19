4984079cf4448e2679aeb8c0e61d8e8b19adee09
P1ccione <simone.piscozzo@gmail.com>
2023-04-19 11:34:05 +0200

    js catalogo per modificare gli src in base al modello completato

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -6,9 +6,12 @@ if (variabile === null) {
     localStorage.setItem('modello', 'uomo');
   // La variabile non esiste ancora nel localStorage e viene settata a uomo
 } else {
-    console.log(variabile);
-    const categories = document.querySelectorAll('.category img')
-    categories.forEach()
+    const tShirtCategory = document.querySelector('#t-shirt')
+    const longSleevedTShirtCategory = document.querySelector('#long-sleeved-t-shirt')
+    const canottaCategory = document.querySelector('#canotta')
+    tShirtCategory.src = `../img/${variabile}/T-shirt/immagine-t-shirt-${variabile}-bianca.png`;
+    longSleevedTShirtCategory.src = `../img/${variabile}/long-sleeved-t-shirt/immagine-long-sleeved-t-shirt-${variabile}-bianca.png`;
+    canottaCategory.src = `../img/${variabile}/Canotta/immagine-canotta-${variabile}-bianca.png`;
   // La variabile esiste già nel localStorage, puoi utilizzarne il suo valore   donna uomo o bambino
 
 }
