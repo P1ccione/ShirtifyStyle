686cabb7ae9dcf73c6ba856648bd295a2dcf61fa
P1ccione <simone.piscozzo@gmail.com>
2023-04-25 18:17:39 +0200

    perfezionato algoritmo della selezione del colore e della taglia

--- scripts/t-shirt-modify.js
+++ scripts/t-shirt-modify.js
@@ -3,31 +3,91 @@ const productSelected = JSON.parse(localStorage.getItem("productSelected"));
 
 const productImage = document.querySelector(".t-shirt-img");
 const priceSpan = document.querySelector("#price-span");
-let colorSelect = document.querySelector("#color");
-let sizeSelect = document.querySelector("#size");
+const arrayColors = [];
+const arraySizes = [];
+const colorSelect = document.querySelector("#color");
+const sizeSelect = document.querySelector("#size");
 let newProductSelectedt = productSelected;
+const imageFile = document.querySelector("#file");
+const imageTshirt = document.querySelector("#personal-img");
+const quantityInput = document.querySelector("#quantity");
 
 priceSpan.innerHTML = `${productSelected.prezzo} €`;
 productImage.style.backgroundImage = `url(${productSelected.img}`;
 
-const imageFile = document.querySelector("#file");
-const imageTshirt = document.querySelector("#personal-img");
-
-for (let i = 0; i < colorSelect.options.length; i++) {
-  if (colorSelect.options[i].value === productSelected.colore) {
-    colorSelect.selectedIndex = i;
-    break;
+function changeOption() {
+  for (let i = 0; i < colorSelect.options.length; i++) {
+    if (colorSelect.options[i].value === newProductSelectedt.colore) {
+      colorSelect.selectedIndex = i;
+      break;
+    }
+  }
+  for (let i = 0; i < sizeSelect.options.length; i++) {
+    if (sizeSelect.options[i].value === newProductSelectedt.taglia) {
+      sizeSelect.selectedIndex = i;
+      break;
+    }
   }
 }
 
-for (let i = 0; i < sizeSelect.options.length; i++) {
-  if (sizeSelect.options[i].value === productSelected.taglia) {
-    sizeSelect.selectedIndex = i;
-    break;
+function changeColorsSizes() {
+  const arrayColors = [];
+  const arraySizes = [];
+  productsArray.forEach((product) => {
+    if (
+      product.modello == newProductSelectedt.modello &&
+      product.categoria == newProductSelectedt.categoria &&
+      product.taglia == newProductSelectedt.taglia &&
+      !(product.colore in arrayColors)
+    ) {
+      arrayColors.push(product.colore);
+    }
+    if (
+      product.modello == newProductSelectedt.modello &&
+      product.categoria == newProductSelectedt.categoria &&
+      product.colore == newProductSelectedt.colore &&
+      !(product.taglia in arraySizes)
+    ) {
+      arraySizes.push(product.taglia);
+    }
+  });
+  colorSelect.innerHTML = "";
+  for (let i = 0; i < arrayColors.length; i++) {
+    const option = document.createElement("option");
+    option.text = arrayColors[i];
+    option.value = arrayColors[i];
+    colorSelect.appendChild(option);
+  }
+  sizeSelect.innerHTML = "";
+  for (let i = 0; i < arraySizes.length; i++) {
+    const option = document.createElement("option");
+    option.text = arraySizes[i];
+    option.value = arraySizes[i];
+    sizeSelect.appendChild(option);
   }
 }
 
-sizeSelect = document.querySelector("#size");
+function showProduct() {
+  const colorSelect = document.querySelector("#color");
+  const color = colorSelect.options[colorSelect.selectedIndex].value;
+  const size = sizeSelect.options[sizeSelect.selectedIndex].value;
+  productsArray.forEach((product) => {
+    if (
+      product.modello == newProductSelectedt.modello &&
+      product.categoria == newProductSelectedt.categoria &&
+      product.taglia == size &&
+      product.colore == color
+    ) {
+      productImage.style.backgroundImage = `url(${product.img}`;
+      newProductSelectedt = product;
+      changeColorsSizes();
+      changeOption();
+    }
+  });
+}
+
+changeColorsSizes();
+changeOption();
 
 imageFile.addEventListener("change", () => {
   const file = imageFile.files[0];
@@ -39,55 +99,14 @@ imageFile.addEventListener("change", () => {
 });
 
 colorSelect.addEventListener("change", () => {
-  const colorSelect = document.querySelector("#color");
-  const color = colorSelect.options[colorSelect.selectedIndex].value;
-  let control = false;
-  productsArray.forEach((product) => {
-    if (
-      product.modello == newProductSelectedt.modello &&
-      product.categoria == newProductSelectedt.categoria &&
-      product.taglia == newProductSelectedt.taglia &&
-      product.colore == color
-    ) {
-      productImage.style.backgroundImage = `url(${product.img}`;
-      newProductSelectedt = product;
-      control = true;
-    }
-  });
-  if(control == false) {
-    alert(`Non ci sono ${newProductSelectedt.categoria} per ${newProductSelectedt.modello} di taglia ${newProductSelectedt.taglia} con colore ${color}`)  
-    for (let i = 0; i < colorSelect.options.length; i++) {
-      if (colorSelect.options[i].value === newProductSelectedt.colore) {
-        colorSelect.selectedIndex = i;
-        break;
-      }
-    }
-  }
+  showProduct();
 });
 
 sizeSelect.addEventListener("change", () => {
-  const sizeSelect = document.querySelector("#size");
-  const size = sizeSelect.options[sizeSelect.selectedIndex].value;
-  let control = false;
-  productsArray.forEach((product) => {
-    if (
-      product.modello == newProductSelectedt.modello &&
-      product.categoria == newProductSelectedt.categoria &&
-      product.colore == newProductSelectedt.colore &&
-      product.taglia == size
-    ) {
-      productImage.style.backgroundImage = `url(${product.img}`;
-      newProductSelectedt = product;
-      control = true;
-    }
-  });
-  if(control == false) {
-    alert(`Non ci sono ${newProductSelectedt.categoria} per ${newProductSelectedt.modello} di colore ${newProductSelectedt.colore} con taglia ${size}`)
-    for (let i = 0; i < sizeSelect.options.length; i++) {
-      if (sizeSelect.options[i].value === newProductSelectedt.taglia) {
-        sizeSelect.selectedIndex = i;
-        break;
-      }
-    }
-  }
+  showProduct();
+});
+
+quantityInput.addEventListener("change", () => {
+  const quantity = parseInt(quantityInput.value);
+  priceSpan.innerHTML = `${quantity * newProductSelectedt.prezzo} €`;
 });
