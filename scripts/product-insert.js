31de0c415f59025e787cdd4ea1f8f28cda553248
P1ccione <simone.piscozzo@gmail.com>
2023-05-04 13:07:34 +0200

    implementazione inserimento prodotti

--- scripts/product-insert.js
+++ scripts/product-insert.js
@@ -1,5 +1,13 @@
+let productsArray = JSON.parse(localStorage.getItem("arrayProdotti"));
+if (!productsArray) {
+  const productsArray2 = [];
+  localStorage.setItem("arrayProdotti", JSON.stringify(productsArray2));
+  productsArray = JSON.parse(localStorage.getItem("arrayProdotti"));
+}
+
 const imageFile = document.querySelector("#file");
 const imageProduct = document.querySelector("#immagine-prodotto");
+const form = document.querySelector("form");
 
 imageFile.addEventListener("change", () => {
   const file = imageFile.files[0];
@@ -8,4 +16,38 @@ imageFile.addEventListener("change", () => {
     imageProduct.src = reader.result;
   });
   reader.readAsDataURL(file);
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const file = imageFile.files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    imageProduct.src = reader.result;
+  });
+  const product = {
+    idProdottoCatalogo: Date.now(),
+    colore: document.querySelector("#color").value,
+    taglia: document.querySelector("#size").value,
+    prezzo: document.querySelector("#prezzoProdotto").value,
+    quantita: document.querySelector("#quantitaProdotto").value,
+    materiale: document.querySelector("#materialeProdotto").value,
+    infoSpedizione: document.querySelector("#spedizioneProdotto").value,
+    categoria: document.querySelector("#category").value,
+    img: imageProduct.src,
+    modello: document.querySelector("#model").value,
+  };
+  productsArray.forEach((element) => {
+    if (
+      product.colore == element.colore &&
+      product.taglia == element.taglia &&
+      product.modello == element.modello &&
+      product.categoria == element.categoria
+    ) {
+      alert("ESISTE GIA UN PRODOTTO CON QUESTE CARATTERISTICHE")
+    } else {
+      productsArray.push(product);
+      localStorage.setItem("arrayProdotti", JSON.stringify(productsArray));
+    }
+  });
 });
